e73d5bca9aa2866ce5100a7431ad4f0e7d82a956
bagus hidayat <bagus.hidayat@docotel.com>
2019-12-16 14:38:31 +0700

    fixing naming

---
+++ src/Check.php
@@ -0,0 +1,9 @@
+<?php 
+namespace Numerize;
+
+class Check {
+
+    public static function isGenap(int $angka){
+        return $angka % 2 === 1;
+    } 
+}
--- src/isGenap.php
+++
@@ -1,9 +0,0 @@
-<?php 
-namespace numerize;
-
-class IsGenap {
-
-    public static function isGenap(int $angka){
-        return $angka % 2 === 1;
-    } 
-}
