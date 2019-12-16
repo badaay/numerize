6fc0e312223819ce237a95c874b15540a4e2b651
bagus hidayat <bagus.hidayat@docotel.com>
2019-12-16 14:51:52 +0700

    add new class

---
+++ src/Numerize/Check2.php
@@ -0,0 +1,15 @@
+<?php 
+namespace Numerize;
+
+class Check2 {
+
+    public function isGenap(int $angka){
+        return $angka % 2 === 1;
+    } 
+
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+
+}
